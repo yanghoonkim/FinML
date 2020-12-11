cde0a4a4948337955afbe02703a83c8ef946abb6
yanghoonkim <ad26kt@gmail.com>
2020-12-11 19:37:00 +0900

    Modify getdata.py
    - get_ticker()
        if ticker data exists, load the file

--- finml/data_reader/getdata.py
+++ finml/data_reader/getdata.py
@@ -10,25 +10,36 @@ class GetInitData:
         if not os.path.exists(self.data_path):
             os.makedirs(self.data_path)
     
-    def get_ticker(self, source = 'krx'):
+    def get_ticker(self, source='krx'):
         ''' Get tickers from the given source
         args:
             source: source of data ('krx', ...)
         '''
-        print('Get ticker symbol from [%s] ... ' %source, end='')
         
-        if source == 'krx': # KOSPI / KOSDAQ / KONEX
-            df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
-            
-            # Add 0 padding to tickers
-            df.종목코드 = df.종목코드.map('{:06d}'.format)
-            
-            ticker_path = os.path.join(self.data_path, 'krx')
-            if not os.path.exists(ticker_path): 
-                os.makedirs(ticker_path)
-            
-            # Save 
-            with open(os.path.join(ticker_path, 'kor_ticker'+'.pkl'), 'wb') as f:
-                pkl.dump(df, f)
+        base_path = os.path.join(self.data_path, source)
+        ticker_path = os.path.join(base_path, 'ticker.pkl')
+        
+        if not os.path.exists(ticker_path):
+            print('Get ticker symbol from [%s] ... ' %source, end='')
+        
+            if source == 'krx': # KOSPI / KOSDAQ / KONEX
+                df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
+
+                # Add 0 padding to tickers
+                df.종목코드 = df.종목코드.map('{:06d}'.format)
+
+                if not os.path.exists(base_path): 
+                    os.makedirs(base_path)
+
+                # Save 
+                with open(ticker_path, 'wb') as f:
+                    pkl.dump(df, f)
                 
-        print('Complete!')
+                self.ticker = df
+
+            print('Complete!')
+            
+        else: 
+            print('Load ticker: %s' %ticker_path)
+            with open(ticker_path, 'rb') as f:
+                self.ticker = pkl.load(f)
