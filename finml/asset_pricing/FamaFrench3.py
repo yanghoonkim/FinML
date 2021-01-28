973608e6000723979ca6430648617b8d6d419e05
yanghoonkim <ad26kt@gmail.com>
2021-01-28 15:34:09 +0900

    Add Fama-Macbeth regression

--- finml/asset_pricing/FamaFrench3.py
+++ finml/asset_pricing/FamaFrench3.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from finml.utils import GoogleDriveDownloader
 import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
+from linearmodels.asset_pricing import LinearFactorModel
 
 plt.style.use('ggplot')
 
@@ -74,3 +75,38 @@ def FamaFrench3_sklearn_lr(ff3, ticker_return, plot_return=False):
 
     print('| Coef\t|\tMKT\t|\tSMB\t|\tHML\t|')
     print('|\t|\t%.3f\t|\t%.3f\t|\t%.3f\t|'%(mlr.coef_[0][0], mlr.coef_[0][1], mlr.coef_[0][2]))
+    
+
+def FamaMacbeth(market, tickers, plot_return=False):
+    ''' Implementation of Fama-Macbeth regression
+    args:
+
+    '''
+    # Fama French 3 factor in korea daily return (kospi & kosdaq)
+    # expected_return = rf + beta_mkt * (rm - rf) + beta_smb * SMB + beta_hml * HML
+
+    # Download factor data (data may not be accurate)
+    print('Download factor data (data may not be accurate)...')
+    file_id = '10VLyoL0YO7Q_jPW_TjXf4LC2ZLt5FQtU'
+    destination = 'data/ff3_kospi_kosdaq_kor.csv'
+    GoogleDriveDownloader(file_id, destination)
+
+    ff3 = pd.read_csv(destination)
+
+    ff3 = ff3.set_index(keys=ff3.columns[0])
+    ff3.index = [datetime.strptime(idx, '%Y-%m-%d') for idx in ff3.index]
+    ff3.index.name='Date'
+    ff3 = ff3.rename(columns={'Mkt-Rf': 'Mkt'})
+    ff3 = ff3.drop(['Rf'], axis=1)
+    ff3_m = ff3.resample('m').ffill().pct_change()
+    
+    portfolio_returns = market.calculate_returns(interval = 'm', subset=tickers)
+
+    intersection = ff3_m.index.intersection(portfolio_returns.index)
+    portfolio_returns_ = portfolio_returns.loc[intersection, :]
+    ff3_m_ = ff3_m.loc[intersection, :]
+    
+    mod = LinearFactorModel(portfolios=portfolio_returns_, 
+                        factors=ff3_m_)
+    res = mod.fit()
+    print(res)
